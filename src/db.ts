351abeb1489eb0b6df9bc9e5e183dae175e2fdee
King-Lota <lottanna47@gmail.com>
2026-01-20 16:13:28 +0100

    Update environment variables in .env.development, modify database connection in db.ts, and add cross-env dependency to package.json and package-lock.json

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI as string);
+        const conn = await mongoose.connect(process.env.MONGO_URI! as string);
         console.log(`📦 MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error("❌ DB Connection Failed", error);
