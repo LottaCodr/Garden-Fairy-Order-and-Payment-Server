30ab0a95385602f72e94e9db9adf8e4e4915e3cd
King-Lota <lottanna47@gmail.com>
2026-01-29 12:32:30 +0100

    Update payment routes to include initialization endpoint and reorganize webhook handling for Flutterwave integration.

--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { idempotency } from "@src/middlewares/idempotency.middleware";
 import { flutterwaveWebhook } from "@src/controllers/webhook.controller";
+import { initializePayment } from "@src/controllers/payment.controller";
 
 const router = Router();
-router.post("/flutterwave", idempotency, flutterwaveWebhook);
+router.post("/initialize", initializePayment);
+router.post("/webhook/flutterwave", idempotency, flutterwaveWebhook);
 export default router;
